730575747fcaa5a92a1ea2c9f8b4a6c6ea254e16
Christian Schulze <christian.schulze@mni.thm.de>
2014-04-11 13:55:44 +0200

    added controller

---
+++ AIBitcoinCentralExchangeAPIController/AIBitcoinCentralExchangeAPIController/AIBitcoinCentralExchangeAPIController/AIBitcoinCentralExchangeAPIController.h
@@ -0,0 +1,13 @@
+//
+//  AIBitcoinCentralExchangeAPIController.h
+//  AIBitcoinCentralExchangeAPIController
+//
+//  Created by X on 11.04.14.
+//  Copyright (c) 2014 andinfinity. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AIBitcoinCentralExchangeAPIController : NSObject
+
+@end
---
+++ AIBitcoinCentralExchangeAPIController/AIBitcoinCentralExchangeAPIController/AIBitcoinCentralExchangeAPIController/AIBitcoinCentralExchangeAPIController.m
@@ -0,0 +1,13 @@
+//
+//  AIBitcoinCentralExchangeAPIController.m
+//  AIBitcoinCentralExchangeAPIController
+//
+//  Created by X on 11.04.14.
+//  Copyright (c) 2014 andinfinity. All rights reserved.
+//
+
+#import "AIBitcoinCentralExchangeAPIController.h"
+
+@implementation AIBitcoinCentralExchangeAPIController
+
+@end
